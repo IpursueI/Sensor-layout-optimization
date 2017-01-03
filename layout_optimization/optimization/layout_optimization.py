da32180f2f5a46f06e17730178d6f432f9f409ac
PerryHuang <pengjianghuang@163.com>
2017-01-04 01:24:34 +0800

    记录energy变化状态

--- layout_optimization/optimization/layout_optimization.py
+++ layout_optimization/optimization/layout_optimization.py
@@ -24,6 +24,7 @@ class LayoutOptimization(Annealer):
         self.each_sensor_number = each_sensor_number
         self.pos_file_path = pos_file_path
         self.goal = goal
+        self.weight = 0
         self.sensor_num = ['10728412','10728515','10728382','10728506','10728402','10728400','10728435','10728517','10728383',
         '10728534','10728432','10728401','10728391','10728437','10728525','10728399','10728518','10728442','10728527','10728390',
         '10728405','10728419','10728425','10728439','10728404','10728408','10728522','10728396','10728422','10728507','10728513',
@@ -33,8 +34,7 @@ class LayoutOptimization(Annealer):
 
     def set_sensor_weight(self):
         #这几个传感器的权重为2，其余传感器的权重为1
-        weight = {2:set(['10728399', '10728518', '10728442', '10728527', '10728390', '10728405', '10728419', '10728425', '10728439', '10728404', 
-                    '10728513', '10728387', '10728385', '10728519'])}
+        weight = {2:set(['10728405', '10728390', '10728527']), 3:set(['10728519','10728385','10728387'])}
 
         self.sensor_weight = {}
         for item in self.sensor_num:
@@ -59,18 +59,13 @@ class LayoutOptimization(Annealer):
                 break
         return unselected[final-1]
 
-    def move(self):    #根据权重进行状态转移
+    def move(self):   #随机进行状态转移
         a = random.choice(self.state)
         unselected = list(set(range(self.total_number)).difference(set(self.state)))
-        b = self.weight_select(unselected)
-        self.state.remove(a)
-        self.state.append(b)
-        #print("state: ",self.state)
-
-    def move2(self):   #随机进行状态转移
-        a = random.choice(self.state)
-        unselected = list(set(range(self.total_number)).difference(set(self.state)))
-        b = random.choice(unselected)
+        if self.weight == 0:
+            b = random.choice(unselected)
+        elif self.weight == 1:
+            b = b = self.weight_select(unselected)
         self.state.remove(a)
         self.state.append(b)
 
@@ -82,10 +77,12 @@ class LayoutOptimization(Annealer):
         #print(self.state)
 
         res = rmse.temperature_error() + rmse.humidity_error()
-        # if res <= self.goal:
-        #     super(LayoutOptimization, self).set_user_exit(0,0)
+        if res <= self.goal:
+            super(LayoutOptimization, self).set_user_exit(0,0)
         return res
 
+    def make_weight(self, weight):
+        self.weight = weight
 
 if __name__ == '__main__':
     # state = random.sample(range(34), 2)
@@ -106,49 +103,26 @@ if __name__ == '__main__':
     # drawer = drawLayout('../data/pos/pos.csv','../draw/sensor.png',sensors)
     # drawer.main()
 
-    # res = []
-    # for i in range(3):
-    #     state = random.sample(range(34), 15+i*5)
-    #     lay_opt = LayoutOptimization(state, 34, "../data/filter_data","../data/pos/pos.csv", 10)
-    #     lay_opt.copy_strategy = "slice"
-    #     lay_opt.steps = 10000
-    #     lay_opt.updates = 100
-    #     state, e = lay_opt.anneal()
-    #     state = sorted(state)
-    #     print("root_mean_square_error: %f" % e)
-    #     print("final state: ", state)
-    #     print("-----end-----")
-    #     sensors = [lay_opt.sensor_num[item] for item in state]
-    #     sensors.append(str(e))
-    #     print("sensors:", sensors)
-    #     res.append(sensors)
-
-    # csvfile = file('../data/result/layout_res.csv','wb')
-    # writer = csv.writer(csvfile)
-    # writer.writerows(res)
-    # csvfile.close()
-
+    final_error = [2.69, 2.46, 1.97, 1.66, 1.49, 1.08, 1.08, 1.10, 1.03, 1.03]
     res = []
+    for i in range(10):
+        state = random.sample(range(34), 3+i)
 
-    state = random.sample(range(34), 4)
-    lay_opt = LayoutOptimization(state, 34, "../data/filter_data","../data/pos/pos.csv", 10, 2.46)
-    lay_opt.set_sensor_weight()
-    lay_opt.copy_strategy = "slice"
-    lay_opt.save_state_on_exit = False
-    lay_opt.steps = 100
-    lay_opt.updates = 100
-    state, e = lay_opt.anneal()
-    state = sorted(state)
-    print("root_mean_square_error: %f" % e)
-    print("final state: ", state)
-    print lay_opt.get_energy_record()
-    print("-----end-----")
-    # sensors = [lay_opt.sensor_num[item] for item in state]
-    # sensors.append(str(e))
-    # print("sensors:", sensors)
-    # res.append(sensors)
+        for j in range(2):
 
-    # csvfile = file('../data/result/layout_res.csv','wb')
-    # writer = csv.writer(csvfile)
-    # writer.writerows(res)
-    # csvfile.close()
+            lay_opt = LayoutOptimization(state, 34, "../data/filter_data","../data/pos/pos.csv", 10, final_error[i])
+            lay_opt.set_sensor_weight()      # 设置传感器权重
+            lay_opt.make_weight(j)
+            lay_opt.copy_strategy = "slice"
+            lay_opt.steps = 10000
+            lay_opt.updates = 100
+            
+            state, e = lay_opt.anneal()
+            energy_record =  lay_opt.get_energy_record()
+            res.append(energy_record)
+            print("-----end-----")
+
+    csvfile = file('../data/result/energy_record.csv','wb')
+    writer = csv.writer(csvfile)
+    writer.writerows(res)
+    csvfile.close()
